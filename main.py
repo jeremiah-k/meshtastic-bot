886831b423b00ca04580e4b3da09dccb8996556c
Jeremiah K <17190268+jeremiah-k@users.noreply.github.com>
2023-11-23 13:03:58 -0600

    Convert all docs to use Document class

--- main.py
+++ main.py
@@ -1,19 +1,28 @@
 import os
 from langchain.llms import Ollama
+from langchain.docstore.document import Document
 from langchain.document_loaders import TextLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.embeddings import GPT4AllEmbeddings
 from langchain.vectorstores import chroma
 from langchain.chains import RetrievalQA
 
+
 # Replace 'base_url' with the actual Ollama base URL
-ollama = Ollama(base_url='http://localhost:11434', model="llama2")
+ollama = Ollama(base_url='http://localhost:11434', model="mistral-openorca")
 
 # Path to the local directory containing the .mdx files
 docs_path = 'meshtastic/docs/'
 
+# Convert all_documents to use the Document class
+all_documents = {
+    file_path: Document(page_content=f.read(), metadata={})
+    for subdir, dirs, files in os.walk(docs_path)
+    for file in files if file.endswith('.mdx')
+    for file_path in [os.path.join(subdir, file)]
+}
+
 # Recursively find all .mdx files and load their content
-all_documents = {}
 for subdir, dirs, files in os.walk(docs_path):
     for file in files:
         if file.endswith('.mdx'):
@@ -23,10 +32,10 @@ for subdir, dirs, files in os.walk(docs_path):
 
 # Split documents into chunks
 text_splitter = RecursiveCharacterTextSplitter(chunk_size=500, chunk_overlap=20)
-all_splits = {path: text_splitter.split_documents(doc) for path, doc in all_documents.items()}
+all_splits = text_splitter.split_documents(all_documents.values())
 
 # Flatten the splits for embedding
-all_splits_flat = [chunk for chunks in all_splits.values() for chunk in chunks]
+all_splits_flat = [chunk.page_content for chunk in all_splits]
 
 # Create embeddings
 embedder = GPT4AllEmbeddings()
